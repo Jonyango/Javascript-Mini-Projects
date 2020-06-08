9d5ca20430883b52c5674e0bdc9d5e1005252469
Jonyango <jeanadagi@gmail.com>
2020-06-08 04:39:00 +0200

    Added main functionalities

--- TodoApp/main.js
+++ TodoApp/main.js
@@ -23,7 +23,11 @@ function init(){
     }  
     
     if(todoList){
-        markComplete();
+        todoList.addEventListener('click',markComplete,false);
+    }
+    
+    if(todoList){
+        todoList.addEventListener('click',removeTodo,false)
     }
     
 }
@@ -37,6 +41,18 @@ function createList(){
     const listTitle=document.createElement("input");
     const list=document.createElement("Div");
     
+    // creating an empty state for the application
+    const emptyState=document.createElement("div");
+    const emptyStateTitle=document.createElement("h4");
+    const emptyStateDescription=document.createElement("p");
+    
+    emptyStateTitle.innerText="Add your first Todo";
+    emptyStateDescription.innerText="What do you want to get done today?"
+
+    emptyStateDescription.classList.add('empty-state__description');
+    emptyStateTitle.classList.add('empty-state__title');
+    emptyState.classList.add('empty-state');
+    
     listTitle.type='text';
     listTitle.placeholder='Title';
     
@@ -48,18 +64,26 @@ function createList(){
     addItemButton.classList.add('add-item');
     addItemButton.innerHTML=createIcon();
 
-    
+    emptyState.appendChild(emptyStateTitle);
+    emptyState.appendChild(emptyStateDescription);
 
+    
     list.appendChild(listTitle);
     list.appendChild(addItemButton);
+    list.appendChild(emptyState);
     listContainer.appendChild(list);
     overallContainer.appendChild(listContainer)
     document.body.appendChild(overallContainer);
 
     // Adding an event listener to the addItem buttom.
     addItemButton.addEventListener('click',displayInputForm.bind(null,list),false);
-        
-    }
+
+   
+}
+
+
+   
+
 
 
 
@@ -77,8 +101,11 @@ function addTask(text,list){
    todoList.insertAdjacentHTML('beforeend',
    `<li class="todo-item" data-key="${todo.id}">
    <input id="${todo.id}" type="checkbox"/>
-   <label for="${todo.id}" class="task-tick"></label>
-   <span>${todo.text}</span>`);
+   <label for="${todo.id}" class="tick task-tick"></label>
+   <span>${todo.text}</span>
+   <button class="delete-todo js-delete-todo">
+   <i class="material-icons">delete</i>
+  </button>`);
 
    list.appendChild(todoList);
 
@@ -151,27 +178,34 @@ function toggleComplete(key){
  * Extracts the data key of where the click comes from if it is a checkbox.
  * Passes the data-key to a toggle function that marks the task as complete.
  */
-function markComplete(){
-    console.log('I am inside this function')
-    todoList.addEventListener('click',event=>{
+function markComplete(event){
+
+    if(event.target.classList.contains('task-tick')){
         const itemKey=event.target.parentElement.dataset.key;
-            console.log(itemKey);
-            toggleComplete(itemKey);
-      
-       if(event.target.classList.contains('task-tick')){
-            return true;
+        console.log(itemKey);
+        toggleComplete(itemKey);
+  
             }
-    
-    });
+
 }
-
-
+    
 
 // a function that will help us delete the todo items.
+function deleteTodo(key) {
+    todoTasks = todoTasks.filter(item => item.id !== Number(key));
+    const item = document.querySelector(`[data-key='${key}']`);
+    item.remove();
+  }
 
-function deleteTodoItem(){
-    
+
+function removeTodo(event){
+    if (event.target.classList.contains('js-delete-todo')) {
+        const itemKey = event.target.parentElement.dataset.key;
+        deleteTodo(itemKey);
+      }
+
 }
+
 
 
 init()
